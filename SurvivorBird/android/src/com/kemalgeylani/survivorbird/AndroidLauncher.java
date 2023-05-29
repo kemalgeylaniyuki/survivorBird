b9aa9e778d67028ff0e6d5cbb9159b5e9705b169
Kemal Geylani YUKİ <96909207+kemalgeylaniyuki@users.noreply.github.com>
2023-05-29 09:35:42 +0300

    survivorBird

---
+++ SurvivorBird/android/src/com/kemalgeylani/survivorbird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kemalgeylani.survivorbird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kemalgeylani.survivorbird.Survivor Bird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Survivor Bird(), config);
+	}
+}
